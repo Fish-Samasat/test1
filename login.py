f941da504b79cd1ff3dce44fe24d00b44fd19d8b
Fernando <92642424+Fish-Samasat@users.noreply.github.com>
2023-11-10 19:33:32 -0500

    Update login.py
    agregar mysql

--- login.py
+++ login.py
@@ -1,7 +1,7 @@
 
 # Importamos la librería para trabajar con bases de datos SQLite
 import sqlite3
-
+import mysql
 # Creamos una conexión a la base de datos
 conn = sqlite3.connect('usuarios.db')
 
